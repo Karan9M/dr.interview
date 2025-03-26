8f9c92ef384c03ac4d72cbc3f55cf7407bcd15e2
Karan mavadiya <karanmavadiya38@gmail.com>
2025-03-26 19:11:32 +0530

    interview page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from "@/components/Agent"
+
+const page = () => {
+  return (
+    <>
+    <h3>Interview Genration</h3>
+    <Agent userName="you" userId="user1" type="generate"/>
+    </>
+  )
+}
+
+export default page
